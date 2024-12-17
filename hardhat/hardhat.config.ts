1ed4e8cf8067a2baf8bda5aab7b444e30f71e355
Tommaso Paulon <paulon.tommaso@gmail.com>
2024-12-17 22:08:23 +0000

    fix: add network besu to hardhat

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  networks: {
+    besu : {
+      url: "http://besu:8545"
+    }
+  }
 };
 
 export default config;
