45d7cc395473d38eb6c7ac66feb09938c2cc9e24
Tommaso Paulon <paulon.tommaso@gmail.com>
2025-01-08 14:58:49 +0000

    feat: make ERC1155 tokens soulbound

--- hardhat/contracts/Attestor.sol
+++ hardhat/contracts/Attestor.sol
@@ -39,5 +39,21 @@ contract Attestor is ERC1155, Ownable {
         Reclaim(reclaimAddress).verifyProof(proof);
         _mint(account, id, 1, data);
     }
+
+     function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+        // Allow minting (transfer from zero address)
+        if (from != address(0)) {
+            revert("Transfer not allowed");
+        }
+        return;
+    }
    
 }
